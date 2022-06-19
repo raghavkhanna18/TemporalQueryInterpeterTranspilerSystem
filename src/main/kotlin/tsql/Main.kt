32d65ef73b1fc2ad796716a3640dc1b9da31419c
Raghav Khanna <37894186+raghavkhanna18@users.noreply.github.com>
2022-06-19 14:43:46 +0000

    Add Commandline Parser

--- src/main/kotlin/tsql/Main.kt
+++ src/main/kotlin/tsql/Main.kt
@@ -1,5 +1,7 @@
 package tsql
 
+import com.xenomachina.argparser.ArgParser
+import com.xenomachina.argparser.default
 import com.xenomachina.argparser.mainBody
 import tsql.ast.constructAndCreateAST
 import tsql.ast.nodes.ProgramAST
@@ -7,33 +9,44 @@ import tsql.ast.symbol_table.SymbolTable
 import tsql.database.Table
 import tsql.error.CommonErrorPrinter
 import tsql.error.ErrorAccumulator
-import tsql.error.SyntaxErrorListener
 import java.io.File
 
 fun main(args: Array<String>) = mainBody {
     Main.main(args)
 }
 
+class ProgramArgs(parser: ArgParser) {
+    val filepath by parser.storing(
+        "-f", "--file",
+        help = "filepath to print output to"
+    ).default("")
+
+    val transpile by parser.flagging(
+        "-t", "--transpile",
+        help = "Should transpiler be used"
+    )
+    val query by parser.positional("Query to be executed")
+}
+
 object Main {
-
     fun main(args: Array<String>) {
-        val transpile = true
-        val input =
-            "SELECT a.artist, a.rank, b.title, b.rank FROM  top_100_artists as 'a' UNTIL JOIN top_100_songs as 'b' on a.artist = b.artist WHERE a.artist = 'BTS' ;"
-        val syntaxErrorAccumulator = ErrorAccumulator(
-            Constants.SYNTAX_EXIT_CODE,
-            CommonErrorPrinter(arrayOf(input))
-        )
+        ArgParser(args).parseInto(::ProgramArgs).run {
+            val input = query
+            val syntaxErrorAccumulator = ErrorAccumulator(
+                Constants.SYNTAX_EXIT_CODE,
+                CommonErrorPrinter(arrayOf(input))
+            )
 
-        val (absSynTree, symbolTable) = constructAndCreateAST(
-            syntaxErrorAccumulator,
-            input
-        )
+            val (absSynTree, symbolTable) = constructAndCreateAST(
+                syntaxErrorAccumulator,
+                input
+            )
 
-        if (transpile) {
-           print(transpile(absSynTree, symbolTable))
-        } else {
-            execute(absSynTree, symbolTable).print()
+            if (transpile) {
+                print(transpile(absSynTree, symbolTable))
+            } else {
+                execute(absSynTree, symbolTable).print()
+            }
         }
     }
 
